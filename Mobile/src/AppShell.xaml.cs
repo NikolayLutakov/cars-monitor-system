349b4bce7018899c859d28983309068da46e914d
Nikolay Lyutakov <nikolaylutakov@gmail.com>
2024-03-12 16:33:19 +0200

    Initial

---
+++ Mobile/src/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CarsMonitorSystemMobileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Server/src/CarsAPI/Controllers/CarsController.cs
@@ -0,0 +1,15 @@
+﻿using CarsAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarsAPI.Controllers
+{
+    [Route("api/[controller]/[action]/")]
+    public class CarsController : Controller
+    {
+        public async Task<IActionResult> Test()
+        {
+            
+            return Ok();
+        }
+    }
+}
